56b355d10e50883131fae501dde2d46d5a641c09
Bart van den Heuvel <bvandenh@redhat.com>
2021-09-21 13:14:35 +0200

    added egress logic

--- demo/egress.sh
+++ demo/egress.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-cat << EOF >> ../deploy/simpson-and-bouvier/homer.simpson/container-helper.yaml
+members=( "marge.simpson" "homer.simpson" "selma.bouvier" "patty.bouvier" )
+
+for i in
+cat << EOF >> ../deploy/simpson-and-bouvier/$i/container-helper.yaml
   - name: xs4all.nl
     host: www.xs4all.nl
     port: 80
   - name: router
     host: 192.178.2.254
-    port: 443
+    port: 80
 EOF
