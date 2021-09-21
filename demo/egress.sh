f86eb2e3eb640ac0ed3e1060a78690f9075c9459
root <root@ocp4-bastion.cnv.example.com>
2021-09-21 10:01:11 +0000

    added egress targets

--- demo/egress.sh
+++ demo/egress.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-cat << EOF >>
+cat << EOF >> ../deploy/simpson-and-bouvier/homer.simpson/container-helper.yaml
+  - name: xs4all.nl
+    host: www.xs4all.nl
+    port: 80
+  - name: router
+    host: 192.178.2.254
+    port: 443
+EOF
